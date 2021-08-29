b7167cfe29697c066d6bd32f936e5c06861c4f54
ShahabAthar25 <shahabathar25@gmail.com>
2021-08-29 10:53:59 +0500

    Made Website More Responsive

--- src/Component/Portfolio/About/About.js
+++ src/Component/Portfolio/About/About.js
@@ -2,30 +2,19 @@ import React from 'react'
 
 import profilePic from '../../../images/profile_pic.jpeg'
 
-import ProgressBar from './ProgressBar/ProgressBar'
-
 import './About.css'
 
 function About() {
     return (
         <div className="about" id="about">
-            <div className="profile">
-                <img src={profilePic} alt="" className="profile_pic" />
-            </div>
-            <div className="aboutme">
-                <div className="header">
-                    <h1>About Me</h1>
+            <div className="aboutme__container">
+                <div className="about__upper">
+                    <div className="about__porfile_pic"></div>
+                    <div className="about__contact"></div>
                 </div>
-                <div className="seprater"></div>
-                <div className="description">
-                    <div className="text">
-                        Hello i am Shahab Athar, I am a full stack web developer, GUI developer, and Data Analyst
-                    </div>
-                    <div className="skills">
-                        <ProgressBar pct="80" text="Full Stack Web Developer" />
-                        <ProgressBar pct="65" text="GUI Developer" />
-                        <ProgressBar pct="85" text="Data Analyst" />
-                    </div>
+                <div className="about__lower">
+                    <div className="about__left"></div>
+                    <div className="about__right"></div>
                 </div>
             </div>
         </div>
